2b9ecf2435f944dd787323931a4f1f609a81870d
Diego89git <dtoapanta1240@uta.edu.ec>
2023-11-06 09:18:44 -0500

    Creacion del modulo presentacion

---
+++ app_famortiza/src/app/presentacion/presentacion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PresentacionRoutingModule } from './presentacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PresentacionRoutingModule
+  ]
+})
+export class PresentacionModule { }
